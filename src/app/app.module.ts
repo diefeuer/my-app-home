8805c6a967f78b5bffd0f9f41aafd5ae93b30fff
Mauricio Gomez <mau.gomez.21@gmail.com>
2019-10-26 01:13:48 -0300

    estructura de body

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { bodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    bodyComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
